6c969f86101eab791d5d5084d33ce9fff6cb6188
shahalcm <shahalcm9@gmail.com>
2026-06-22 14:34:07 +0530

    clean updates

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import "./src/polyfill";
-import "react-native-gesture-handler";
+// import "./src/polyfill";
+// import "react-native-gesture-handler";
 import React from "react";
 import { LogBox } from "react-native";
 

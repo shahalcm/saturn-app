23bf6580e8cadf30d56862b2e086830d3eaad594
shahalcm <shahalcm9@gmail.com>
2026-06-22 11:55:02 +0530

    given commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { AuthProvider } from "./context/AuthContext";
-import { UserProvider } from "./context/UserContext";
-import { LanguageProvider } from "./context/LanguageContext";
-import { AppNavigator } from "./navigation/AppNavigator";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { View, Text } from "react-native";
 
 export default function App() {
-  console.log("STEP_2_APP");
-  React.useEffect(() => {
-    console.log("🚀 [Lifecycle Log]: APP_MOUNTED");
-  }, []);
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ErrorBoundary>
-        <SafeAreaProvider>
-          <AuthProvider>
-            <UserProvider>
-              <LanguageProvider>
-                <AppNavigator />
-              </LanguageProvider>
-            </UserProvider>
-          </AuthProvider>
-        </SafeAreaProvider>
-      </ErrorBoundary>
-    </GestureHandlerRootView>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#FFF" }}>
+      <Text style={{ fontSize: 24, fontWeight: "bold", color: "#000" }}>APK TEST OK</Text>
+    </View>
   );
 }
